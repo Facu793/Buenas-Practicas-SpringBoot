8ce013e08816c19b827272d1ed293d9cb9389234
Facu <Facundocordoba793@gmail.com>
2025-11-21 18:13:11 -0300

    Se creo DTO de la entidad Cliente

---
+++ backend/backend/src/main/java/com/EjBackend/backend/dto/ClienteResponseDTO.java
@@ -0,0 +1,84 @@
+package com.EjBackend.backend.dto;
+
+import java.util.List;
+
+public class ClienteResponseDTO {
+
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private String direccion;
+    private List<MotoResponseDTO> motos;
+
+    public ClienteResponseDTO() {
+    }
+
+    public ClienteResponseDTO(Long id, String nombre, String apellido, String email, String telefono, String direccion, List<MotoResponseDTO> motos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.motos = motos;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public List<MotoResponseDTO> getMotos() {
+        return motos;
+    }
+
+    public void setMotos(List<MotoResponseDTO> motos) {
+        this.motos = motos;
+    }
+}
+
